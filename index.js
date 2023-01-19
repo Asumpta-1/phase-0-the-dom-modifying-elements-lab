34a5e853ee776feb588fb9c60b36e1cf24240d59
asumpta <sumgathoni@gmail.com>
2023-01-19 11:58:28 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const element = document.getElementById('main');
-element.remove();
+// const element = document.getElementById('main');
+// element.remove();
+
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Asumpta is the champion";
